beab1a3caad81c5666667804078e6a7181fe0bb5
CrazzyGamer182 <nirmay.singh.lamba@gmail.com>
2022-10-23 16:41:25 -0700

    Fixed sizing issue for text

--- block_breaker/block.js
+++ block_breaker/block.js
@@ -67,68 +67,68 @@ class Block {
       }
       textAlign(CENTER, CENTER);
       if (this.number.toString().length < 2) {
-        if (this.yes == false) {
+        if (this.yes == false || selected == false) {
           textSize(35 * (wh / 500));
         } else {
           textSize(38 * (wh / 500));
         }
       } else if (this.number.toString().length < 3) {
-        if (this.yes == false) {
+        if (this.yes == false || selected == false) {
           textSize(31 * (wh / 500));
         } else {
           textSize(34 * (wh / 500));
         }
       } else if (this.number.toString().length < 4) {
-        if (this.yes == false) {
+        if (this.yes == false || selected == false) {
           textSize(27 * (wh / 500));
         } else {
           textSize(30 * (wh / 500));
         }
       } else if (this.number.toString().length < 5) {
-        if (this.yes == false) {
+        if (this.yes == false || selected == false) {
           textSize(23 * (wh / 500));
         } else {
           textSize(26 * (wh / 500));
         }
       } else if (this.number.toString().length < 6) {
-        if (this.yes == false) {
+        if (this.yes == false || selected == false) {
           textSize(19 * (wh / 500));
         } else {
           textSize(22 * (wh / 500));
         }
       }
       else if (this.number.toString().length < 7) {
-        if (this.yes == false) {
+        if (this.yes == false || selected == false) {
           textSize(15 * (wh / 500));
         } else {
           textSize(18 * (wh / 500));
         }
       } else if (this.number.toString().length < 8) {
-        if (this.yes == false) {
+        if (this.yes == false || selected == false) {
           textSize(12 * (wh / 500));
         } else {
           textSize(15 * (wh / 500));
         }
       } else if (this.number.toString().length < 9) {
-        if (this.yes == false) {
+        if (this.yes == false || selected == false) {
           textSize(10 * (wh / 500));
         } else {
           textSize(13 * (wh / 500));
         }
       } else if (this.number.toString().length < 10) {
-        if (this.yes == false) {
+        if (this.yes == fals || selected == falsee) {
           textSize(8 * (wh / 500));
         } else {
           textSize(11 * (wh / 500));
         }
       } else if (this.number.toString().length < 11) {
-        if (this.yes == false) {
+        if (this.yes == false || selected == false) {
           textSize(6 * (wh / 500));
         } else {
           textSize(9 * (wh / 500));
         }
       } else if (this.number.toString().length < 12) {
-        if (this.yes == false) {
+        if (this.yes == false || selected == false) {
           textSize(5 * (wh / 500));
         } else {
           textSize(8 * (wh / 500));
