cd3a8341eeed16ea9e0ab4957691ab13fafbd1fd
CrazzyGamer182 <nirmay.singh.lamba@gmail.com>
2022-11-19 19:52:24 -0800

    Finished Mathar.io

--- test/public/sketch1.js
+++ test/public/sketch1.js
@@ -5,11 +5,11 @@ let s = 10000;
 let socket;
 let players = {};
 let game = 0;
-let namee = "";
+let namee = "Player";
 let num;
 let gs = 10000;
 let fontt;
-let c;
+let c = '#ed225d';
 let one;
 let two;
 let wait = 0;
@@ -54,11 +54,17 @@ function setup() {
   socket = io('https://api.thegamebox.ca');
   socket.on('mouse', newPlayer);
   textAlign(CENTER, CENTER);
-  var inp = createInput('').attribute('maxlength', 10);
-  inp.position(windowWidth/2 - 50*(wh/500), windowHeight/2 - 40*(ht/300));
+  if (getItem("name") != null) {
+    namee = getItem("name");
+  }
+  if (getItem("color1") != null) {
+    c = color(getItem("color1"), getItem("color2"), getItem("color3"));
+  }
+  var inp = createInput(namee).attribute('maxlength', 10);
+  inp.position(windowWidth/2 - 50*(wh/500), windowHeight/2 - 56*(ht/300));
   inp.size(100*(wh/500), 7*(wh/500));
   inp.input(myInputEvent);
-  colorPicker = createColorPicker('#ed225d');
+  colorPicker = createColorPicker(c);
   colorPicker.size(20*(ht/300), 45*(ht/300));
   colorPicker.position(windowWidth/2 - 60*(ht/300), windowHeight/2 - 20*(ht/300));
   num = getItem('id');
@@ -105,6 +111,7 @@ function draw() {
     fill(200);
     rect(425, 225, 75, 75);
     leaderBoard = [];
+    let miniMapSize = 1000;
     for (let i = 0; i < b.length; i++) {
       leaderBoard.push(players[b[i]]);
     }
@@ -123,8 +130,8 @@ function draw() {
         text(leaderBoard[i].n, leaderBoard[i].x - x + 250, leaderBoard[i].y - y + 150)
         fill(225, 25, 50);
         noStroke();
-        if (leaderBoard[i].x > -5000 && leaderBoard[i].y > -5000 && leaderBoard[i].x < 5000 && leaderBoard[i].y < 5000) {
-          circle(leaderBoard[i].x*0.0075 + 462.5, leaderBoard[i].y*0.0075 + 262.5, 50*0.075*leaderBoard[i].s/10000);
+        if (leaderBoard[i].x-x > -miniMapSize && leaderBoard[i].y-y > -miniMapSize && leaderBoard[i].x-x < miniMapSize && leaderBoard[i].y-y < miniMapSize) {
+          circle(leaderBoard[i].x*(75/miniMapSize) + 462.5, leaderBoard[i].y*(75/miniMapSize) + 262.5, 50*(75/miniMapSize)*leaderBoard[i].s/10000);
         }
         leaderBoard[i].show();
       } else {
@@ -142,9 +149,8 @@ function draw() {
       }
     }
     fill(25, 50, 225);
-    if (x < 5000 && x > -5000 && y > -5000 && y < 5000) {
-      circle(x*0.0075 + 462.5, y*0.0075 + 262.5, 50*0.075*s/10000);
-    }
+    noStroke();
+    circle(0 + 462.5, 0 + 262.5, 50*(75/miniMapSize)*s/10000);
     if (gs > s) {
       s += 10;
     } else if (gs < s) {
@@ -227,9 +233,11 @@ function draw() {
     c = colorPicker.color()
     fill(0);
     noStroke();
-    textSize(40);
-    text("Agar.io", 250, 45)
+    textSize(33);
+    text("Mathar.io", 250, 45)
     textSize(11);
+    push();
+    translate(0, -15);
     text("Choose name and color", 250, 95)
     stroke(red(c)-50, green(c)-50, blue(c)-50);
     fill(c);
@@ -244,7 +252,7 @@ function draw() {
     stroke(5, 150, 10);
     strokeWeight(2);
     strokeJoin(ROUND);
-    if (mouseX > 225*(wh/500) && mouseX < 275*(wh/500) && mouseY > 185*(wh/500) && mouseY < 205*(wh/500)) {
+    if (mouseX > 225*(wh/500) && mouseX < 275*(wh/500) && mouseY+15 > 185*(wh/500) && mouseY+15 < 205*(wh/500)) {
       rect(222.5, 183.75, 55, 22.5, 3, 3);
       textSize(12);
     } else {
@@ -259,47 +267,123 @@ function draw() {
     stroke(50);
     strokeWeight(2);
     push();
+    textAlign(CENTER, CENTER);
     translate(0, 55);
+    push();
+    translate(0, 5);
     if (gameMode.includes("add")) {
       fill(234, 203, 93);
+      stroke(50);
       rect(230, 180, 32, 32, 5, 5);
-    } else if (mouseX > 214*(wh/500) && mouseX < 246*(wh/500) && mouseY > 219*(wh/500) && mouseY < 251*(wh/500)) {
+      fill(0);
+      noStroke();
+      textSize(25);
+      text("+", 230, 178);
+    } else if (mouseX > 214*(wh/500) && mouseX < 246*(wh/500) && mouseY+10 > 219*(wh/500) && mouseY+10 < 251*(wh/500)) {
       fill(125);
+      stroke(50);
       rect(230, 180, 35, 35, 5, 5);
+      fill(0);
+      noStroke();
+      textSize(30);
+      text("+", 230, 178);
     } else {
       fill(100);
+      stroke(50);
       rect(230, 180, 32, 32, 5, 5);
+      fill(0);
+      noStroke();
+      textSize(25);
+      text("+", 230, 178);
     }
     if (gameMode.includes("sub")) {
       fill(234, 203, 93);
+      stroke(50);
       rect(270, 180, 32, 32, 5, 5);
-    } else if (mouseX > 254*(wh/500) && mouseX < 286*(wh/500) && mouseY > 219*(wh/500) && mouseY < 251*(wh/500)) {
+      fill(0);
+      noStroke();
+      textSize(25);
+      text("-", 270, 177);
+    } else if (mouseX > 254*(wh/500) && mouseX < 286*(wh/500) && mouseY+10 > 219*(wh/500) && mouseY+10 < 251*(wh/500)) {
       fill(125);
+      stroke(50);
       rect(270, 180, 35, 35, 5, 5);
+      fill(0);
+      noStroke();
+      textSize(30);
+      text("-", 270, 177);
     } else {
       fill(100);
+      stroke(50);
       rect(270, 180, 32, 32, 5, 5);
+      fill(0);
+      noStroke();
+      textSize(25);
+      text("-", 270, 177);
     }
     if (gameMode.includes("mul")) {
       fill(234, 203, 93);
+      stroke(50);
       rect(230, 220, 32, 32, 5, 5);
-    } else if (mouseX > 214*(wh/500) && mouseX < 246*(wh/500) && mouseY > 259*(wh/500) && mouseY < 291*(wh/500)) {
+      fill(0);
+      strokeWeight(0.5);
+      stroke(0);
+      textSize(20);
+      text("x", 230, 217);
+    } else if (mouseX > 214*(wh/500) && mouseX < 246*(wh/500) && mouseY+10 > 259*(wh/500) && mouseY+10 < 291*(wh/500)) {
       fill(125);
+      stroke(50);
+      strokeWeight(2);
       rect(230, 220, 35, 35, 5, 5);
+      stroke(0);
+      strokeWeight(0.5);
+      textSize(25);
+      fill(0);
+      text("x", 230, 217);
     } else {
       fill(100);
+      stroke(50);
+      strokeWeight(2);
       rect(230, 220, 32, 32, 5, 5);
+      stroke(0);
+      strokeWeight(0.5);
+      textSize(20);
+      fill(0);
+      text("x", 230, 217);
     }
     if (gameMode.includes("div")) {
       fill(234, 203, 93);
+      stroke(50);
+      strokeWeight(2);
       rect(270, 220, 32, 32, 5, 5);
-    } else if (mouseX > 254*(wh/500) && mouseX < 286*(wh/500) && mouseY > 259*(wh/500) && mouseY < 291*(wh/500)) {
+      stroke(0);
+      strokeWeight(0.5);
+      textSize(23);
+      fill(0);
+      text("÷", 270, 219);
+    } else if (mouseX > 254*(wh/500) && mouseX < 286*(wh/500) && mouseY+10 > 259*(wh/500) && mouseY+10 < 291*(wh/500)) {
       fill(125);
+      stroke(50);
+      strokeWeight(2);
       rect(270, 220, 35, 35, 5, 5);
+      stroke(0);
+      strokeWeight(0.5);
+      textSize(28);
+      fill(0);
+      text("÷", 270, 219);
     } else {
       fill(100);
+      stroke(50);
+      strokeWeight(2);
       rect(270, 220, 32, 32, 5, 5);
+      stroke(0);
+      strokeWeight(0.5);
+      textSize(23);
+      fill(0);
+      text("÷", 270, 219);
     }
+    pop();
+    pop();
     pop();
     rectMode(CORNER);
   } else {
@@ -380,8 +464,8 @@ class Pellet {
         this.color1 = random(50, 255);
         this.color2 = random(50, 255);
       }
-    } else if (x > -400 && x < 400 && y > -300 && y < 300) {
-      if (int(random(3000)) == 999 && x > -400 && x < 400 && y > -300 && y < 300) {
+    } else if (this.x > -400 && this.x < 400 && this.y > -300 && this.y < 300) {
+      if (int(random(350)) == 100 && x > -400 && x < 400 && y > -300 && y < 300) {
         if (choose == "add") {
           this.number = one + two;
         } else if (choose == "sub") {
@@ -430,13 +514,17 @@ class SimplePellet {
 
 function mousePressed() {
   if (game == 0) {
-    if (mouseX > 225*(wh/500) && mouseX < 275*(wh/500) && mouseY > 185*(wh/500) && mouseY < 205*(wh/500)) {
+    if (mouseX > 225*(wh/500) && mouseX < 275*(wh/500) && mouseY+15 > 185*(wh/500) && mouseY+15 < 205*(wh/500)) {
       game = 1;
       c = colorPicker.color()
       removeElements();
       newNums();
+      storeItem("name", namee);
+      storeItem("color1", red(c));
+      storeItem("color2", green(c));
+      storeItem("color3", blue(c));
     }
-    if (mouseX > 214*(wh/500) && mouseX < 246*(wh/500) && mouseY > 219*(wh/500) && mouseY < 251*(wh/500)) {
+    if (mouseX > 214*(wh/500) && mouseX < 246*(wh/500) && mouseY+10 > 219*(wh/500) && mouseY+10 < 251*(wh/500)) {
       if (gameMode.includes("add")) {
         if (gameMode.length > 1)
           gameMode.splice(gameMode.indexOf("add"), 1);
@@ -444,7 +532,7 @@ function mousePressed() {
         gameMode.push("add");
       }
     }
-    if (mouseX > 254*(wh/500) && mouseX < 286*(wh/500) && mouseY > 219*(wh/500) && mouseY < 251*(wh/500)) {
+    if (mouseX > 254*(wh/500) && mouseX < 286*(wh/500) && mouseY+10 > 219*(wh/500) && mouseY+10 < 251*(wh/500)) {
       if (gameMode.includes("sub")) {
         if (gameMode.length > 1)
           gameMode.splice(gameMode.indexOf("sub"), 1);
@@ -452,7 +540,7 @@ function mousePressed() {
         gameMode.push("sub");
       }
     }
-    if (mouseX > 214*(wh/500) && mouseX < 246*(wh/500) && mouseY > 259*(wh/500) && mouseY < 291*(wh/500)) {
+    if (mouseX > 214*(wh/500) && mouseX < 246*(wh/500) && mouseY+10 > 259*(wh/500) && mouseY+10 < 291*(wh/500)) {
       if (gameMode.includes("mul")) {
         if (gameMode.length > 1)
           gameMode.splice(gameMode.indexOf("mul"), 1);
@@ -460,7 +548,7 @@ function mousePressed() {
         gameMode.push("mul");
       }
     }
-    if (mouseX > 254*(wh/500) && mouseX < 286*(wh/500) && mouseY > 259*(wh/500) && mouseY < 291*(wh/500)) {
+    if (mouseX > 254*(wh/500) && mouseX < 286*(wh/500) && mouseY+10 > 259*(wh/500) && mouseY+10 < 291*(wh/500)) {
       if (gameMode.includes("div")) {
         if (gameMode.length > 1)
           gameMode.splice(gameMode.indexOf("div"), 1);
@@ -579,6 +667,8 @@ function eat() {
         game = 2;
         let x = 0;
         let y = 0;
+      } else if (s > players[b[i]].s) {
+        s += players[b[i]].s/3;
       }
     }
   }
