db93d1d8f57acacd519eda8166172d42422447a3
Nirmay Singh <nirmay.singh.lamba@gmail.com>
2025-02-05 17:37:41 -0800

    new

--- viktoriia/sketch.js
+++ viktoriia/sketch.js
@@ -1,70 +1,192 @@
-let num = 1;
-let phrase = " ";
-let  phrases = [
-  " ",
-  "You always make me feel loved",
-  "You're honest with me",
-  "You call me spiderman",
-  "You always care about me, if I'm okay, upset, etc.",
-  "Because you let me be your baby",
-  "Because you're my little baby",
-  "You always reassure me about things even when you don't wanna talk about it",
-  "You're sooo cute and small",
-  "You listen to me when I talk about random stuff",
-  "You send me cats and say it’s us                                      (it is us)",
-  "You deal with me being busy and you're okay with it",
-  "You tell me when you're upset",
-  "You don't give up on the relationship when you get upset",
-  "When you get upset you’re patient with me",
-  "You play Minecraft with me even though you didn’t want to",
-  "You use your nails on me",
-  "You try Indian food even though it’s spicy for you",
-  "You give me so many kisses",
-  "You don't get mad when I fall asleep while talking",
-  "You say ew and stuff about other guys",
-  "You're the best girlfriend ever",
-];
+let buttons = [];
+let screen = 0;
+let font;
+let scrollValue = 0;
+let done = 0;
+let gox;
+let goy;
+let photoCount = 10;
 
-function setup() {
-  createCanvas(800, 500);
-  prepare();
-  textAlign(CENTER, CENTER);
-  textWrap(WORD);
-  if (month() == 7) {
-    num = day() - 17;
-  } else {
-    num = day() + 14;
-  }
-  phrase = phrases[num];
-  textFont(fontt);
-  background(random(155)+100, random(155)+100, random(155)+100);
-  textSize(wh/15);
-  text("Why I love you", wh/2, ht*(1/4));
-  textSize(wh/20);
-  text("Reason #" + num + ":", wh/2, ht*(2/5));
-  textSize(wh/25);
-  text(phrase, 0, ht*(3/5), wh);
-  if (day() == 25) {
-    imageMode(CENTER);
-    image(pet, ht*(4.35/5), wh/2, pet.width/(wh/300), pet.height/(wh/300))
-  }
+function changeScreen(s) {
+  screen++;
 }
+
+let images = [];
 
 function preload() {
-  pet = loadImage("pet.png");
-  fontt = loadFont("Comfortaa-Regular.ttf");
+  heartImage = loadImage("backgroundheart.png");
+  font = loadFont("monster.ttf");
+  kids = loadImage("kids.png");
+  dates = loadImage("dates.png");
+  plans = loadImage("plans.png");
+  v1 = loadImage("v1.png");
+  v2 = loadImage("v2.png");
+  
+  // Load images for picsScreen
+  for (let i = 0; i < photoCount; i++) {
+    images[i] = loadImage(i + ".jpg");
+  }
 }
 
-function prepare() {
-  if (800 * (windowHeight / 500) < windowWidth) {
-    wh = 800 * (windowHeight / 500);
+function setup() {
+  if (500 * (windowHeight / 300) < windowWidth) {
+    wh = 500 * (windowHeight / 300);
     ht = windowHeight;
   } else {
     wh = windowWidth;
-    ht = 500 * (windowWidth / 800);
+    ht = 300 * (windowWidth / 500);
   }
   cnv = createCanvas(wh, ht);
   centerCanvas();
+  rectMode(CENTER);
+  textAlign(CENTER, CENTER);
+  textFont(font);
+  newButton("Start", 250, 195, 100, 40, 250, 150, 150, changeScreen, 0);
+  newButton(
+    "Photos",
+    28 + 45,
+    35,
+    90,
+    35,
+    250,
+    150,
+    150,
+    function () {
+      screen = 2;
+    },
+    1
+  );
+  newButton(
+    "Back",
+    40,
+    25,
+    60,
+    30,
+    250,
+    150,
+    150,
+    function () {
+      screen = 1;
+    },
+    4
+  );
+  newButton(
+    "Back",
+    40,
+    25,
+    60,
+    30,
+    250,
+    150,
+    150,
+    function () {
+      screen = 1;
+    },
+    3
+  );
+  newButton(
+    "Back",
+    40,
+    25,
+    60,
+    30,
+    250,
+    150,
+    150,
+    function () {
+      screen = 1;
+    },
+    2
+  );
+  newButton(
+    "Back",
+    40,
+    25,
+    60,
+    30,
+    250,
+    150,
+    150,
+    function () {
+      screen = 1;
+    },
+    5
+  );
+  newButton(
+    "Dates",
+    28 * 2 + 90 + 45,
+    35,
+    90,
+    35,
+    250,
+    150,
+    150,
+    function () {
+      screen = 3;
+    },
+    1
+  );
+  newButton(
+    "Family",
+    28 * 3 + 90 * 2 + 45,
+    35,
+    90,
+    35,
+    250,
+    150,
+    150,
+    function () {
+      screen = 4;
+    },
+    1
+  );
+  newButton(
+    "Plans",
+    28 * 4 + 90 * 3 + 45,
+    35,
+    90,
+    35,
+    250,
+    150,
+    150,
+    function () {
+      screen = 5;
+    },
+    1
+  );
+  newButton(
+    "Yes",
+    90,
+    230,
+    90,
+    35,
+    250,
+    150,
+    150,
+    function () {
+      done = 1;
+      buttons.pop();
+      buttons.pop();
+    },
+    1
+  );
+  newButton(
+    "No",
+    210,
+    230,
+    90,
+    35,
+    250,
+    150,
+    150,
+    function () {
+      gox = random(150, 400);
+      goy = random(100, 250);
+    },
+    1
+  );
+  gox = buttons[buttons.length - 1].x;
+  goy = buttons[buttons.length - 1].y;
 }
 
 function centerCanvas() {
@@ -72,3 +194,158 @@ function centerCanvas() {
   this.cy = (windowHeight - height) / 2;
   cnv.position(cx, cy);
 }
+
+function draw() {
+  scale(width / 500, height / 300);
+  makeBackground();
+  if (screen == 4) {
+    familyScreen();
+  } else if (screen == 3) {
+    datesScreen();
+  } else if (screen == 5) {
+    plansScreen();
+  } else if (screen == 2) {
+    picsScreen();
+  } else if (screen == 1) {
+    askScreen();
+  }
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].show(mouseX / (width / 500), mouseY / (height / 300));
+  }
+  if (done == 0) {
+    buttons[buttons.length - 1].x += (gox - buttons[buttons.length - 1].x) / 5;
+    buttons[buttons.length - 1].y += (goy - buttons[buttons.length - 1].y) / 5;
+  }
+}
+
+function askScreen() {
+  if (done == 0) {
+    image(v1, 0, -10, 500, 300);
+  } else {
+    image(v2, 0, -10, 500, 300);
+  }
+}
+
+function datesScreen() {
+  image(dates, 10, 0, 500, 300);
+}
+
+function plansScreen() {
+  image(plans, 10, 0, 500, 300);
+}
+
+function familyScreen() {
+  image(kids, 115, -scrollValue, 300, 500);
+  rect(490, 15 + scrollValue / 0.9, 3, 20, 2, 2);
+}
+
+function mouseWheel(event) {
+  scrollValue += event.delta;
+  if (scrollValue > 223) {
+    scrollValue = 223;
+  }
+  if (scrollValue < 0) {
+    scrollValue = 0;
+  }
+}
+
+function makeBackground() {
+  image(heartImage, 0, -5, 200, 200);
+  image(heartImage, 200, -5, 200, 200);
+  image(heartImage, 400, -5, 200, 200);
+  image(heartImage, 0, 195, 200, 200);
+  image(heartImage, 200, 195, 200, 200);
+  image(heartImage, 400, 195, 200, 200);
+  image(heartImage, 0, 0, 500, 300);
+  fill(255, 100);
+  rect(250, 150, 500, 300);
+}
+
+class Button {
+  constructor(t, x, y, w, h, r, g, b, c, s) {
+    this.t = t;
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.c = c;
+    this.s = s;
+  }
+  show(mx, my) {
+    if (screen == this.s) {
+      if (
+        mx > this.x - this.w / 2 &&
+        mx < this.x + this.w / 2 &&
+        my > this.y - this.h / 2 &&
+        my < this.y + this.h / 2
+      ) {
+        fill(this.r + 30, this.g + 30, this.b + 30);
+      } else {
+        fill(this.r, this.g, this.b);
+      }
+      stroke(150, 50, 50);
+      strokeWeight(5);
+      rect(this.x, this.y, this.w, this.h, 5, 5);
+      strokeWeight(0.5);
+      stroke(0);
+      fill(0);
+      text(this.t, this.x, this.y - 2, this.w, this.h);
+    }
+  }
+  pressed(mx, my) {
+    if (
+      mx > this.x - this.w / 2 &&
+      mx < this.x + this.w / 2 &&
+      my > this.y - this.h / 2 &&
+      my < this.y + this.h / 2 &&
+      this.s == screen
+    ) {
+      this.c();
+      return true;
+    }
+  }
+}
+
+function newButton(t, x, y, w, h, r, g, b, c, s) {
+  buttons.push(new Button(t, x, y, w, h, r, g, b, c, s));
+}
+
+function mousePressed() {
+  for (let i = 0; i < buttons.length; i++) {
+    if (buttons[i].pressed(mouseX / (width / 500), mouseY / (height / 300))) {
+      return;
+    }
+  }
+}
+
+function picsScreen() {
+  let cols = 4;
+  let rows = Math.ceil(photoCount / cols);
+  let spacing = Math.min(300 / cols, 500 / rows);
+  let cropSize, cropX, cropY;
+  push();
+  translate(110, 33);
+  for (let i = 0; i < photoCount; i++) {
+    let x = (i % cols) * spacing + spacing / 2;
+    let y = Math.floor(i / cols) * spacing + spacing / 2;
+    
+    // Define cropping dimensions (square aspect ratio)
+    let img = images[i];
+    let minSide = Math.min(img.width, img.height);
+    cropSize = minSide;
+    cropX = (img.width - cropSize) / 2;
+    cropY = (img.height - cropSize) / 2;
+    rectMode(CORNER)
+    strokeWeight(3);
+    rect(x - spacing / 2 + 5, y - spacing / 2 + 5, spacing - 10, spacing - 10);
+    rectMode(CENTER)
+    image(img, x - spacing / 2 + 5, y - spacing / 2 + 5, spacing - 10, spacing - 10, cropX, cropY, cropSize, cropSize);
+  }
+  pop();
+}
+
+
+
